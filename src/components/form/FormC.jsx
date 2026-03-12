555eea264e8e5e5f5d0d3f58cb39dd0ef0e5e093
unknown <andresperlo5@gmail.com>
2026-03-11 21:40:31 -0300

    11-03 state

--- src/components/form/FormC.jsx
+++ src/components/form/FormC.jsx
@@ -1,98 +1,187 @@
-import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Swal from "sweetalert2";
 
 const FormC = ({ idPage }) => {
-  //Estados 
-  let formulario = {}
+  //Estados
+  const [formulario, setFormulario] = useState({
+    nombreUsuario: "",
+    correoUsuario: "",
+    contrasenia: "",
+    repContrasenia: "",
+    checkUsuario: false,
+    rolUsuario: "usuario",
+    login: false,
+    bloqueo: false,
+  });
+
+  const [formLog, setFormLog] = useState({
+    nombreUsuario: "",
+    contrasenia: "",
+  });
+
+  /*  
+   const [nombreUsuario, setNombreUsuario] = useState("");
+  const [emailUsuario, setEmailUsuarioo] = useState("");
+  const [contrasenia, setContrasenia] = useState("");
+  const [repContrasenia, setRepContrasenia] = useState("");
+  const [checkUsuario, setCheckUsuario] = useState(false); */
 
   const handleChangeRegister = (ev) => {
-    formulario = {
-      [ev.target.name]: ev.target.value
-    }
-  }
+    setFormulario({
+      ...formulario,
+      [ev.target.name]:
+        ev.target.type === "checkbox" ? ev.target.checked : ev.target.value,
+    });
+  };
 
   const handleClickBottomRegister = (ev) => {
-    ev.preventDefault()
-    console.log(formulario)
+    ev.preventDefault();
+    const {
+      nombreUsuario,
+      emailUsuairo,
+      contrasenia,
+      repContrasenia,
+      checkUsuario,
+    } = formulario;
 
-  }
+    if (contrasenia && repContrasenia && contrasenia === repContrasenia) {
+      Swal.fire({
+        title: "Registro Exitoso!",
+        text: "En breve seras redirigido a iniciar tu sesion!",
+        icon: "success",
+      });
+    }
+  };
 
+  const handleChangeLogin = (ev) => {
+    const { nombreUsuario, contrasenia } = formLog;
+  };
+
+  const handleClickBottomLog = (ev) => {
+    ev.preventDefault();
+    console.log(formLog);
+  };
 
   return (
     <>
-      {
-        idPage === "login"
-          ?
-          <Form className='w-25'>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Nombre de Usuario</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+      {idPage === "login" ? (
+        <Form className="w-25">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Nombre de Usuario</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter email"
+              onChange={handleChangeLogin}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter email"
+              onChange={handleChangeLogin}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-            <Container className='d-flex justify-content-center'>
-              <Button variant="primary" type="submit" className='w-75'>
-                Ingresar
-              </Button>
-            </Container>
-          </Form>
+          <Container className="d-flex justify-content-center">
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-75"
+              onClick={handleClickBottomLog}
+            >
+              Ingresar
+            </Button>
+          </Container>
+        </Form>
+      ) : (
+        <Form className="w-25">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Nombre de Usuario</Form.Label>
+            <Form.Control
+              value={formulario.nombreUsuario}
+              type="text"
+              name="nombreUsuario"
+              placeholder="Enter email"
+              /* onChange={(ev) => setNombreUsuario(ev.target.value)} */
+              onChange={handleChangeRegister}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-          :
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo del Usuario</Form.Label>
+            <Form.Control
+              value={formulario.correoUsuario}
+              type="email"
+              name="correoUsuario"
+              placeholder="Enter email"
+              onChange={handleChangeRegister}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-          <Form className='w-25'>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Nombre de Usuario</Form.Label>
-              <Form.Control type="text" name='nombreUsuario' placeholder="Enter email" onChange={handleChangeRegister} />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              type="password"
+              value={formulario.contrasenia}
+              name="contrasenia"
+              placeholder="Enter email"
+              onChange={handleChangeRegister}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Correo del Usuario</Form.Label>
-              <Form.Control type="email" name="correoUsuario" placeholder="Enter email" onChange={handleChangeRegister} />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" name='contrasenia' placeholder="Enter email" onChange={handleChangeRegister} />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Repetir Contraseña</Form.Label>
-              <Form.Control type="password" name='repContrasenia' placeholder="Password" onChange={handleChangeRegister} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Aceptar terminos y condiciones" onChange={handleChangeRegister} name='checkUsuario' />
-            </Form.Group>
-            <Container className='d-flex justify-content-center'>
-              <Button variant="primary" type="submit" className='w-75' onClick={handleClickBottomRegister}>
-                Enviar Datos
-              </Button>
-            </Container>
-          </Form>
-
-      }
-
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Repetir Contraseña</Form.Label>
+            <Form.Control
+              type="password"
+              value={formulario.repContrasenia}
+              name="repContrasenia"
+              placeholder="Password"
+              onChange={handleChangeRegister}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              type="checkbox"
+              value={formulario.checkUsuario}
+              label="Aceptar terminos y condiciones"
+              onChange={handleChangeRegister}
+              name="checkUsuario"
+            />
+          </Form.Group>
+          <Container className="d-flex justify-content-center">
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-75"
+              onClick={handleClickBottomRegister}
+            >
+              Enviar Datos
+            </Button>
+          </Container>
+        </Form>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default FormC
+export default FormC;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,41 @@
-import CardC from "../components/card/CardC"
-import CarouselC from "../components/carousel/CarouselC"
+import { useEffect, useState } from "react";
+import CardC from "../components/card/CardC";
+import CarouselC from "../components/carousel/CarouselC";
 
 const HomePage = () => {
+  const [productos, setProductos] = useState([]);
+
+  const obtenerProductosFakeStore = async () => {
+    const productosApi = await fetch("https://fakestoreapi.com/products");
+    const productos = await productosApi.json();
+    localStorage.setItem("productos", JSON.stringify(productos));
+    setProductos(productos);
+  };
+
+  // ciclos de vida de un componente
+  //inicializa - monta - actualiza - desmonta
+
+  //componente se inicializa y se monta
+  useEffect(() => {
+    obtenerProductosFakeStore();
+  }, []);
+
+  //componente se actualiza
+  /*    useEffect(() => {
+      obtenerProductosFakeStore();
+    }, [productos, todos los estados que uds quieran]); */
+
+  //componente se desmonta
+
+  /*       useEffect(() => {
+       return(
+       () => {
+          }
+       )
+      }, []); */
+
   return (
     <>
-
       <CarouselC />
 
       <main>
@@ -16,12 +47,9 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-
-
-
       </main>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
